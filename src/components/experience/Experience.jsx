9f1601f817f1af2edebcb9cd20d2f16be3bc030d
ahmedzaki98 <ahmedzaki19998@gmail.com>
2022-12-27 01:36:04 +0200

    Added Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,83 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <div>
-      Experience
-    </div>
-  )
-}
+    <section id="experience">
+      <h5>What skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Skillful</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Skillful</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>JavaScript (ES6)</h4>
+                <small className="text-light">Skillful</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>React.js</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>Vue.js</h4>
+                <small className="text-light">Skillful</small>
+              </div>
+            </article>
+          </div>
+        </div>
 
-export default Experience
+        <div className="experience_Languages">
+          <h3>Languages Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Skillful</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon " />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Skillful</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
