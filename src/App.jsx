410e10ac7bf2855a4c8422e3f1a36143f02eb162
ahmedzaki98 <ahmedzaki19998@gmail.com>
2022-12-28 18:25:26 +0200

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/layout/header/Header";
+import Nav from "./components/layout/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+// import Testimonials from "./components/tetimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/layout/footer/Footer";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <React.Fragment>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      {/* <Testimonials /> */}
+      <Contact />
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
